8424289bfa29fd9af07e68e5174ccb3758f38569
whiskj <wkhsisks66@gmail.com>
2024-09-15 08:47:47 +0100

    Sun Sep 15 08:47:47 AM WAT 2024

--- git_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cm=$(date)
-
-git add .
-git commit -m "$cm"
-git branch -M main
-git push -u origin main
--- gitssh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "eval \$(ssh-agent -s)"
-echo "ssh-add ~/whiskgit"
-echo "ssh-add -l -E sha256"
